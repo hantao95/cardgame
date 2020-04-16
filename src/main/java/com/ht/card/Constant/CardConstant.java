e4d7993fdc9c223489c1d77d933004d9b9524e3f
hantao95 <471834687@qq.com>
2020-04-16 10:39:45 +0800

    添加常量

--- src/main/java/com/ht/card/Constant/CardConstant.java
+++ src/main/java/com/ht/card/Constant/CardConstant.java
@@ -10,5 +10,22 @@ public class CardConstant {
     public static String messagetype_chat = "chat";
     /**准备**/
     public static String messagetype_ready = "ready";
+    /**抢地主**/
+    public static String getMessagetype_landowner="landowner";
+    /**出牌**/
+    public static String messagetype_play="play";
+
+    //****************花色*****************//
+    /**黑桃**/
+    public static  String cardType_Spades = "Spades";
+    /**红心**/
+    public static  String cardType_Heart = "Heart";
+    /**草花**/
+    public static  String cardType_Flower = "Flower";
+    /**方块**/
+    public static  String cardType_Orthogon = "Orthogon";
+    /**王**/
+    public static  String cardType_King = "King";
+
 
 }
